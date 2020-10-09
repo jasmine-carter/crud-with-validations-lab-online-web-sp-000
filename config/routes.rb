f86f025c37c233933980fe06323312e08a5e8da7
Jasmine Carter <jasmine.carter87@gmail.com>
2020-10-09 01:54:12 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root "application#index"
 
-  resources :songs, only: [:new, :create, :show, :edit, :update]
+  resources :songs, only: [:new, :create, :show, :edit, :update, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
